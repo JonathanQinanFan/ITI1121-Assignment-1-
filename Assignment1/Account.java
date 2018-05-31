29c1baf76bb0c9402623ddb79630d171f74eca97
praci098 <31967410+praci098@users.noreply.github.com>
2018-05-31 19:40:19 +0000

    This is (4), to be completed
    I've completed step 3 of this section and barely started step 1 and 2.

--- Assignment1/Account.java
+++ Assignment1/Account.java
@@ -1,8 +1,35 @@
 public abstract class Account{
 	
 	Customer customer; 
-	private double balance;
+	protected double balance;
 	private final int INITSIZE = 25;
-	
+	//Not sure how to use this,
 	Transactions[] transactions;
+
+	public Account(Customer c, double b){
+		this.customer = c;
+		this.balance = balance;
+		
+		
+	}
+	//I hate working with arrays so this is not done yet
+	public void reallocate(Transactions[] t){
+	
+	}
+		
+	public double getBalance(){
+		return balance;
+	}
+	
+	public Customer getCustomer(){
+		return customer;
+	}
+	
+	public void setCustomer(Customer c){
+		this.customer = c;
+	}	
+	
+	public String toString(){
+		return("The balance of the account of " + customer + " is " + balance)
+	}
 }
