899ad3fc64947d4183b172a77feb7e353b881922
praci098 <31967410+praci098@users.noreply.github.com>
2018-06-01 13:52:41 -0400

    Added the comments

--- Assignment1/Customer.java
+++ Assignment1/Customer.java
@@ -1,11 +1,13 @@
 public abstract class Customer{
 	
+	//Initializing instance variables
 	protected String firstName;
 	protected String lastName;
 	protected int age;
 	protected int customerNumber;
 	private int lastCustomerNumber = 9999;
 	
+	//Constructor with three parameters. The first and last name of the customer, and his age
 	public Customer(String fn, String ln, int a){
 		firstName = fn;
 		lastName = ln;
@@ -14,10 +16,12 @@ public abstract class Customer{
 		lastCustomerNumber++;
 	}
 	
+	//Getters in order to get the customer number
 	public int getCustomerNumber(){
 		return customerNumber;
 	}
 	
+	//Method equals used to determine if two objects has the same content or description
 	public boolean equals(Customer other){
 		if(!(other instanceof Customer)) {
 			return false;
@@ -26,12 +30,14 @@ public abstract class Customer{
 		return (other.getCustomerNumber() == this.getCustomerNumber());
 	}
 	
+	//toString method used to display the customer information.
 	public String toString(){
 		
 		return ("The name of the customer is " + firstName + " " + lastName + ", age " + age + " and your customer number is " customerNumber);
 		
 	}
 	
+	//Abstract getter methods that will be implplemented in the child class.
 	public abstract double getSavingsInterest();
 	
 	public abstract double getCheckInterest();
