bdd522c269134006c718206725a89e7bae8305ad
praci098 <31967410+praci098@users.noreply.github.com>
2018-06-01 21:49:00 -0400

    Added some comments

--- Assignment1/Senior.java
+++ Assignment1/Senior.java
@@ -1,5 +1,6 @@
 public class Senior extends Customer {
 	
+	//Initializing instance Variables
 	private final double CHECK_INTEREST = 0.04;
 	private final double CHECK_INTEREST_VIP = 0.04;
 	private final double CHECK_CHARGE = 0.01;
@@ -10,11 +11,13 @@ public class Senior extends Customer {
 	private final double OVERDRAFT_PENALTY_VIP;
 	private boolean VIP;
 	
+	//Constructor that has 4 parameter. The first and last name, the age and the VIP status.
 	public Senior (String fn, String ln, int a, boolean VIP){
 		super(fn, ln, a);
 		this.VIP = VIP;
 	}
 	
+	//These getters are overwriting the abstract methods from the parent class
 	public double getSavingsInterest(){
 		return SAVINGS_INTEREST;			
 	}
