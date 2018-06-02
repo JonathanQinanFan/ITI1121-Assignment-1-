88243b360629a768abfd697cb94abc47547a3503
praci098 <31967410+praci098@users.noreply.github.com>
2018-06-01 23:15:29 -0400

    Added comments and added the reallocate method

--- Assignment1/Account.java
+++ Assignment1/Account.java
@@ -1,22 +1,29 @@
 public abstract class Account{
 	
+	//Initializing instance variables
 	Customer customer; 
 	private double balance;
 	private final int INITSIZE = 25;
-	//Not sure how to use this,
 	Transactions[] transactions;
 
+	//Constructor making accounts for a specific customer and balance.
 	public Account(Customer c, double b){
 		this.customer = c;
 		this.balance = balance;
-		
+		transactions = new Transactions[INITSIZE];
 		
 	}
-	//I hate working with arrays so this is not done yet
-	public void reallocate(Transactions[] t){
 	
+	//This method will be called when the array is full and needs more space.
+	public void reallocate(Transactions[] t){
+		int destination = new int[this.transactions.length *2];
+		for (int i=; i<this.transactions.length; i++){
+			destination[i] = this.transactions[i];
+		}
+		this.transactions = destination;
 	}
-		
+	
+	//Getters for the balance and customer
 	public double getBalance(){
 		return balance;
 	}
@@ -25,10 +32,12 @@ public abstract class Account{
 		return customer;
 	}
 	
+	//Setter in order to change customer
 	public void setCustomer(Customer c){
 		this.customer = c;
 	}	
 	
+	//ToString used to return the balance of a customer
 	public String toString(){
 		return("The balance of the account of " + customer + " is " + balance)
 	}
