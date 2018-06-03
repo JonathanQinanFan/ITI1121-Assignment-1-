79453d3af1bd6e2042bddab7a1c2528b999c2506
Jonathan Fan <32018287+JonathanQinanFan@users.noreply.github.com>
2018-06-03 13:41:23 -0400

    Add files via upload

---
+++ Senior.java
@@ -0,0 +1,36 @@
+public class Senior extends Customer {
+	
+	//Initializing instance Variables
+	private final double CHECK_INTEREST = 0.04;
+	private final double CHECK_INTEREST_VIP = 0.04;
+	private final double CHECK_CHARGE = 0.01;
+	private final double CHECK_CHARGE_VIP = 0;
+	private final double SAVINGS_INTEREST = 0.08;
+	private final double SAVINGS_INTEREST_VIP = 0.1;
+	private final double OVERDRAFT_PENALTY = 10;
+	private final double OVERDRAFT_PENALTY_VIP;
+	private boolean VIP;
+	
+	//Constructor that has 4 parameter. The first and last name, the age and the VIP status.
+	public Senior (String firstName, String lastName, int age, boolean VIP){
+		super(firstName, lastName, age);
+		this.VIP = VIP;
+	}
+	
+	//These getters are overwriting the abstract methods from the parent class
+	public double getSavingsInterest(){
+		return SAVINGS_INTEREST;			
+	}
+	
+	public double getCheckInterest(){
+     	return CHECK_INTEREST;   
+ 	}
+	
+	public double getCheckCharge(){
+		return CHECK_CHARGE;
+	}
+}
+
+	
+
+	
