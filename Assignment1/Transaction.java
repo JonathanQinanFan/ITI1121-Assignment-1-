eaf82fc827069bb462a3e7f516246145590ce75e
praci098 <31967410+praci098@users.noreply.github.com>
2018-06-05 13:12:09 +0000

    Update Transaction.java

--- Assignment1/Transaction.java
+++ Assignment1/Transaction.java
@@ -1,14 +1,15 @@
 import java.util.Date;
-
+//This class takes care fo the bank's transactions
 public class Transaction{
-
+	
+	//Initializing instances variables
 	private byte transaction;
 	private double amount, fees;
 	private Date date;
 	private String description;
 	private String transactionType;
 	
-	
+	//Constructor that build an instance of transaction with a specified transaction type, an amount and the fees.
 	public Transaction(byte transaction, double amount, double fees){
 		
 		this.transaction = transaction;
@@ -18,6 +19,7 @@ public class Transaction{
 		
 	}
 	
+	//This method will figure what kind of transaction is the user trying to make. Then, it will displays the information.
 	public String processTransaction(){
 		switch(transaction){
 			case 0:
