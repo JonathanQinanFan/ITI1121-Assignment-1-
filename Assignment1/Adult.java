923f18efba9fadcd0dda536003ec366f9f97bb27
praci098 <31967410+praci098@users.noreply.github.com>
2018-06-05 13:04:41 +0000

    Comments added except 3 lines

--- Assignment1/Adult.java
+++ Assignment1/Adult.java
@@ -1,5 +1,7 @@
+//This class is the Customer's specialization for Adults customers.
 public class Adult extends Customer{
 	
+	//Declaring instance variables
 	private final double SAVINGS_INTEREST = 0.03;
 	private final double CHECK_INTEREST = 0.01;
 	private final double CHECK_CHARGE = 0.03;
@@ -8,22 +10,27 @@ public class Adult extends Customer{
 	private final int Ctype = 1;
 	private boolean VIP = false;
 	
+	//Constructor with 3 parameters. It takes the full name and age of the adult in question
 	public Adult(String firstName, String lastName, int age){
 		super(firstName, lastName, age);
 	}
 	
+	//Getter for the SavingsInterest
 	public double getSavingsInterest(){
 		return SAVINGS_INTEREST; 		
 	} 	
 	
+	//Getter for the CheckInterest
 	public double getCheckInterest(){
 		return CHECK_INTEREST;
 	}
 	
+	//Getter for the CheckCharge
 	public double getCheckCharge(){
 		return CHECK_CHARGE;
 	}
 	
+	//Getter for the Over
 	public double getODP_100(){
 		return OVERDRAFT_PENALTY;
 	}
@@ -31,16 +38,20 @@ public class Adult extends Customer{
 	public double getODP_500(){
 		return OVERDRAFT_PENALTY;
 	}
+	
 	public double getNF(){
 		
 		return NO_FEE;
 	}
+	
+	//Getter for the customer type
 	public int getCustomerType(){
 		
 		return Ctype;
 		
 	}
 	
+	//Getter for the VIP status of adults, which in this case will obviously be false for every adults.
 	public boolean getVIP(){
 		
 		return VIP;
